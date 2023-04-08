5f8cc011264fda8ab2892a83e516e7f4e01d34ec
Ismailmln <126384455+Ismailmln@users.noreply.github.com>
2023-04-08 12:46:18 +0600

    Add files via upload

---
+++ DZ1_Ismail_25-01/main.js
@@ -0,0 +1,33 @@
+//Первое задание
+// var rating = prompt('Пожалуйста оцените наш сервис от 1 до 10')
+// if (rating > 5 ){
+//     alert('Спасибо за высокую оценку')
+// }else if(rating < 5 ){
+//     alert('Мы работаем над улучшением сервиса')
+// }else {
+//     alert('Введите только цифры')
+// }
+//Второе задание
+// var lang = prompt ('Выборы языка и дня')
+// switch (lang){
+//     case 'RUS':
+//         console.log(['ПН,ВТ,СР,ЧТ,ПТ,СБ,ВС'])
+//         alert(["ПН,ВТ,СР,ЧТ,ПТ,СБ,ВС"])
+//         break
+//     case 'ENG':
+//         console.log(['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'])
+//         alert(["'mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'"])
+//         break
+//     default:
+//         alert("Error")
+//         break
+// }
+//
+// var lang = prompt ('Выборы языка и дня')
+// if (lang === 'RUS' ){
+//     console.log(['ПН,ВТ,СР,ЧТ,ПТ,СБ,ВС'])
+// }else if (lang === 'ENG' .trim().toUpperCase()){
+//     console.log(['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'])
+// }else {
+//     alert('Error')
+// }
